4d17334fbb2cc2d957f1f03819c1e5da23071cd8
Ryan Ehrlich <Ryansehrlich@gmail.com>
2017-10-23 21:19:52 -0700

    Refactor

--- matrix.py
+++ matrix.py
@@ -1,19 +1,16 @@
 from __future__ import division
 import math
 
-constArray = [
-	[0., 0., .33],
-	[.4, 0., 0.],
-	[0., .71, .94]
+constMatrix = [
 ]
 
-x = [
-	[900],
-	[500],
-	[2600]
+varMatrix = [
 ]
 
 def multiplyMatrixes(a,b):
+	if len(a) == 0 or len(b) == 0:
+		return []
+
 	result = [[0 for _ in xrange(len(b[0]))] for _ in xrange(len(a))]
 	i = 0
 	j = 0
@@ -22,7 +19,6 @@ def multiplyMatrixes(a,b):
 		while i < len(a):
 			for subIndex in xrange(len(a[i])):
 				result[i][j] += a[i][subIndex] * b[subIndex][j]
-				print a[i][subIndex], b[subIndex][j]
 			i += 1
 		i = 0
 		j += 1
@@ -33,6 +29,6 @@ def multiplyMatrixes(a,b):
 n = 5
 
 for _ in xrange(n):
-	x = multiplyMatrixes(constArray, x)
+	varMatrix = multiplyMatrixes(constMatrix, varMatrix)
 
-print x
+print varMatrix
